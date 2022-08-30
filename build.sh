95ee1843c00453f0ad9f37c5de959ad9c9b2cc5a
xcwan <523067817@qq.com>
2022-08-31 00:02:02 +0800

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-pip uninstall ragdoll -y
-python setup.py bdist_wheel
-pip install --upgrade --force-reinstall dist/*.whl
-pip show -f ragdoll
+# pip uninstall ragdoll -y
+python3 setup.py bdist_wheel
+python3 -m pip install --upgrade --force-reinstall dist/*.whl
+python3 -m pip show -f ragdoll
